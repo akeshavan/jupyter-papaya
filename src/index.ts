4e7c1394e0750d49e92af17d45e371e90945d44d
akeshavan <keshavan@berkeley.edu>
2018-07-31 11:10:44 -0700

    show papaya command on panel

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,49 @@
 import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
-
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
 import '../style/index.css';
-
+import {
+  Widget
+} from '@phosphor/widgets';
 
 /**
  * Initialization data for the nbpapaya-ext extension.
  */
+
 const extension: JupyterLabPlugin<void> = {
   id: 'nbpapaya-ext',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension nbpapaya-ext is activated!');
+
+    // Create a single widget
+    let widget: Widget = new Widget();
+    widget.id = 'nbpapaya-jupyterlab';
+    widget.title.label = 'papaya.js';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'papaya:open';
+    app.commands.addCommand(command, {
+      label: 'papaya viewer',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Tutorial'});
   }
 };
+
 
 export default extension;
